bceea4c2aa71c831b7e0522b209318a72a419289
randazzo-pierre <58936117+randazzo-pierre@users.noreply.github.com>
2020-01-29 16:47:28 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="Voice.css" />
+    <title>Perfect Voice</title>
+</head>
+<body>
+<div id="global">
+    <header>
+        <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a
+    </header>
+    <div id="contenu">
+
+</body>
+</html>
